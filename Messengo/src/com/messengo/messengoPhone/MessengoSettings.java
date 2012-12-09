49031c19e891a7c09acec889bdf74866431f8834
Quentin DOMMERC <dommer.q@gmail.com>
2012-12-09 20:28:00 +0100

    First commit - add structures of preferences

---
+++ Messengo/src/com/messengo/messengoPhone/MessengoSettings.java
@@ -0,0 +1,128 @@
+package com.messengo.messengoPhone;
+
+import java.util.List;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+public class MessengoSettings extends PreferenceActivity {
+
+	private static final boolean ALWAYS_SIMPLE_PREFS = false;
+
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+
+		setupSimplePreferencesScreen();
+	}
+
+
+	@SuppressWarnings("deprecation")
+	private void setupSimplePreferencesScreen() {
+		if (!isSimplePreferences(this)) {
+			return;
+		}
+
+		// In the simplified UI, fragments are not used at all and we instead
+		// use the older PreferenceActivity APIs.
+
+		// Add 'general' preferences.
+		addPreferencesFromResource(R.xml.pref_general);
+
+		PreferenceCategory fakeHeader = new PreferenceCategory(this);
+		fakeHeader.setTitle(R.string.pref_header_data_sync);
+		getPreferenceScreen().addPreference(fakeHeader);
+		addPreferencesFromResource(R.xml.pref_data_sync);
+
+		bindPreferenceSummaryToValue(findPreference("account"));
+		
+		Preference save = (Preference)findPreference("saveSms");
+		save.setOnPreferenceClickListener(saveSms);
+		Preference delete = (Preference)findPreference("clean");
+		delete.setOnPreferenceClickListener(clean);
+	}
+
+	//TODO: Faire les vraies actions :)
+	public OnPreferenceClickListener saveSms = new OnPreferenceClickListener() {
+		
+		@Override
+		public boolean onPreferenceClick(Preference preference) {
+			Toast.makeText(MessengoSettings.this, "Sauvegarde des sms en cours ...", Toast.LENGTH_SHORT).show();
+			return false;
+		}
+	};
+	
+	public OnPreferenceClickListener clean = new OnPreferenceClickListener() {
+		
+		@Override
+		public boolean onPreferenceClick(Preference preference) {
+			Toast.makeText(MessengoSettings.this, "Suppression des données en cours ...", Toast.LENGTH_SHORT).show();
+			return false;
+		}
+	};
+	
+	@Override
+	public boolean onIsMultiPane() {
+		return isXLargeTablet(this) && !isSimplePreferences(this);
+	}
+
+	private static boolean isXLargeTablet(Context context) {
+		return (context.getResources().getConfiguration().screenLayout
+				& Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_XLARGE;
+	}
+
+	private static boolean isSimplePreferences(Context context) {
+		return ALWAYS_SIMPLE_PREFS
+				|| Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB
+				|| !isXLargeTablet(context);
+	}
+
+	@Override
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	public void onBuildHeaders(List<Header> target) {
+		if (!isSimplePreferences(this)) {
+			loadHeadersFromResource(R.xml.pref_headers, target);
+		}
+	}
+
+	private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+		@Override
+		public boolean onPreferenceChange(Preference preference, Object value) {
+			String stringValue = value.toString();
+
+			if (preference instanceof ListPreference) {
+				ListPreference listPreference = (ListPreference) preference;
+				int index = listPreference.findIndexOfValue(stringValue);
+				preference.setSummary(
+						index >= 0
+						? listPreference.getEntries()[index]
+								: null);
+			} else {
+				preference.setSummary(stringValue);
+			}
+			return true;
+		}
+	};
+
+	private static void bindPreferenceSummaryToValue(Preference preference) {
+		preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+		sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+				PreferenceManager
+				.getDefaultSharedPreferences(preference.getContext())
+				.getString(preference.getKey(), ""));
+	}
+
+
+
+}
